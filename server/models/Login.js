5a29b1189e8a5eca7ee88bec5874df0a5ca22591
sai-nath <ssjawadwar@gmail.com>
2024-09-29 10:59:47 +0530

    login Schema

---
+++ server/models/Login.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const loginSchema = new mongoose.Schema(
+  {
+    f_sno: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    f_userName: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    f_Pwd: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+const Login = mongoose.model("Login", loginSchema);
